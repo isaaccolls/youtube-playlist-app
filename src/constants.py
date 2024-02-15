97f7503e52efd5d8b6e1683b3bfc3a8d1e9c0776
Isaac Colls <colls_isaac@hotmail.com>
2024-02-15 12:44:09 -0300

    feat: mapping some playlists

---
+++ src/constants.py
@@ -0,0 +1,30 @@
+playlists = [
+    # 🍭-lolliPop
+    'PL_8z4vyyerkNdojRqG-N2Z2m_Y7MiRuur',
+    # 💚💛❤️-reggae
+    'PL_8z4vyyerkP5JGvkDLUaOjQr5mP3bP2L',
+    # 🍁-420
+    'PL_8z4vyyerkOwxNjLjnL2apDSjerK0-aj',
+    # 🎻-classic
+    'PL_8z4vyyerkMCRjGiM_gwSqQyTjm6uuUd',
+    # 🥫-salsa
+    'PL_8z4vyyerkOA2L_3AUOYQz5PYDYj-cSa',
+    # 🐶-htv
+    'PL_8z4vyyerkOD1YoaRZvjfmMZy1MsOMtD',
+    # ⚡-electro
+    'PL_8z4vyyerkMzbRSUbq6D38pNtTBwN9xw',
+    # 🔫-hiphop
+    'PL_8z4vyyerkOU9rlgZheQg22yAPu5qHHp',
+    # 🤘-rock
+    'PL_8z4vyyerkMh5swg0FKbTFQRujtlhmr-',
+    # 📺⚡-videoElectro
+    'PL_8z4vyyerkMhM-RSZkTpq0zELxXTVyzq',
+    # 📺🐶-videoMixHtv
+    'PL_8z4vyyerkNOgsNi67YVJrfMxaMHiL2u',
+    # 📺💚💛❤️-videoReggae
+    'PL_8z4vyyerkPaPo2QUvvJSwNpxg-XC4CN',
+    # 📺🧘‍♀️-videoLofi
+    'PL_8z4vyyerkOoUnuG_8tPiJGcqKSDeko9',
+    # 📺🤘-videoRock
+    'PL_8z4vyyerkNSD4Q6bBCEqv5OCBHS2LH9',
+]
--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
 from dotenv import load_dotenv
 import os
 from playlist.playlist import Playlist
+from constants import playlists
 
 
 def main():
     load_dotenv()
     api_key = os.getenv('YOUTUBE_API_KEY')
-    playlists = [
-        # 🍭-lolliPop
-        'PL_8z4vyyerkNdojRqG-N2Z2m_Y7MiRuur'
-    ]
     for playlist in playlists:
         Playlist(api_key, playlist).process()
 
