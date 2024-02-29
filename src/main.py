17b0f45e43ff4fd808a562be8f27a237f91bbdc5
Isaac Colls <colls_isaac@hotmail.com>
2024-02-28 21:43:28 -0300

    fix: print

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from constants import playlists
 
 
 def main():
-    print(f'🚀 here we go!')
+    print('🚀 here we go!')
     for playlist_id in playlists:
         Playlist(playlist_id).process()
-    print(f'✅ done!')
+    print('✅ done!')
 
 
 if __name__ == "__main__":
