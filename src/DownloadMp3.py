a05d1a486d6bec4dc34ecebd116a2f362456b00c
Isaac Colls <colls_isaac@hotmail.com>
2025-04-09 18:38:13 -0400

    feat: enhance DownloadMp3 class to support playlist audio downloads

--- src/DownloadMp3.py
+++ src/DownloadMp3.py
@@ -1,16 +1,49 @@
-from pytubefix import YouTube
+# check playlist metadata
+from ytmusicapi import YTMusic
+# download mp3 from youtube
+from pytubefix import YouTube, Playlist
 from pytubefix.cli import on_progress
+# internal project imports
+from constants import playlistIdForMusic, playlistUrlForMusic, pathForMusic
 
 
 class DownloadMp3:
+    def __init__(self):
+        self.playlist_url = playlistUrlForMusic
+        self.playlist_id = str(playlistIdForMusic)
+        self.path = pathForMusic
+
+    def check_playlist(self, playlist_id):
+        print(f"👉 Checking playlist {playlist_id}")
+        playlist_id = str(playlist_id)
+        ytmusic = YTMusic()
+        return ytmusic.get_playlist(playlist_id, 4000, False, 0)
 
     def download_audio(self, url):
         yt = YouTube(url, on_progress_callback=on_progress)
         print(f"👉 start download {yt.title}")
-        ys = yt.streams.get_highest_resolution()
-        ys.download()
+        ys = yt.streams.get_audio_only()
+        ys.download(self.path)
         print(f"✅ download completed {yt.title}")
 
     def run(self):
         print('going for mp3 🔥🚀')
-        self.download_audio("https://www.youtube.com/watch?v=kXYiU_JCYtU")
+        # get playlist info
+        playlist = self.check_playlist(self.playlist_id)
+        # loop through all items in the playlist
+        for content in playlist['tracks']:
+            title = content['title']
+            print(f"👉 title: {title}")
+            artists = content['artists']
+            print(
+                f"👉 artists: {', '.join([artist['name'] for artist in artists])}")
+
+            video_id = content['videoId']
+            print(f"👉 video id: {video_id}")
+
+            # if video_id is None:
+            #     print(f"❌ no video id for {content['title']}")
+            #     continue
+            # video_url = f"https://www.youtube.com/watch?v={video_id}"
+            # print(f"👉 start download {video_url}")
+            # self.download_audio(video_url)
---
+++ src/constants.py
@@ -0,0 +1,5 @@
+playlistIdForMusic = 'PL_8z4vyyerkO0OsXupspGzzw3-XSjC-01'
+playlistUrlForMusic = f'https://www.youtube.com/playlist?list={playlistIdForMusic}'
+playlistUrlForVideo = 'PL_8z4vyyerkMhM-RSZkTpq0zELxXTVyzq'
+pathForMusic = 'download/mp3'
+pathForVideo = 'download/mp4'
