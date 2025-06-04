c3783910e524f87ae24297ba727ddea79d748b01
Isaac Colls <colls_isaac@hotmail.com>
2025-06-04 19:02:46 -0400

    feat: enhance vlcTransfer.sh to include error handling for file uploads

--- vlcTransfer.sh
+++ vlcTransfer.sh
@@ -4,9 +4,12 @@ MP3_DIR="/home/isaac/Projects/youtube-playlist-app/data/mp3"
 UPLOAD_URL="http://192.168.7.140/upload.json"
 
 for file in "$MP3_DIR"/*.mp3; do
-  if [[ -f "$file" ]]; then
+  if [[ -f "$file" && -r "$file" ]]; then
     echo "🚀 Upload: $file"
-    curl -F "files[]=@${file}" "$UPLOAD_URL"
+    LC_ALL=C curl -F "files[]=@${file}" -- "$UPLOAD_URL"
     echo "✅ Uploaded: $file"
+  else
+    echo "❌ No se puede leer el archivo: $file"
   fi
+  echo "----------------------------------------"
 done
